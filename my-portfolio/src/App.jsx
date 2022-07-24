ece3a7a6b42ddd9f2198b8c32570f6ca5dd9a4dd
Subhadip Chowdhury <subhadipchowdhury1998@gmail.com>
2022-07-24 10:35:50 +0530

    first commit

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Header from './Component/Header/Header'
+import Navbar from './Component/Nav/Navbar'
+import About from './Component/About/About'
+import Experience from './Component/Experience/Experience'
+import Services from './Component/Service/Services'
+import Portfolio from './Component/Portfolio/Portfolio'
+import Testimonial from './Component/Testimonial/Testimonial'
+import Contact from './Component/Contact/Contact'
+import Footer from './Component/Footer/Footer'
+// import './index.css'
+
+const App = () => {
+  return (
+    // <div>App</div>
+    <div>
+        <Header/>
+        <Navbar/>
+        <About/>
+        <Experience/>
+        <Services/>
+        <Portfolio/>
+        <Testimonial/>
+        <Contact/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ my-portfolio/src/Component/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import ME from '../../assests/casualme.jpg'
+import HeaderSocial from './HeaderSocial'
+
+const Header = () => {
+  return (
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I'm</h5>
+        <h1>Subhadip Chowdhury</h1>
+        <h5 className='text-light'>React Dveloper</h5>
+        <CTA></CTA>
+        <HeaderSocial/>
+
+        <div className='me'>
+          <img src={ME} alt='me'/>
+        </div>
+
+        <div>
+          <a href='#' className='scroll__down'>Scroll Down</a>
+        </div>
+
+      </div>
+    </header>
+    
+  )
+}
+
+export default Header
